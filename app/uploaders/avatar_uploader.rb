d7818070e914db627a15625d904446ec8e193c8d
M <mpatel.ror@gmail.com>
2013-02-02 15:32:16 +0530

    caf/wav conversion

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,71 @@
+# encoding: utf-8
+require File.join(Rails.root, "lib", "carrier_wave", "ffmpeg")
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::FFMPEG
+
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
+  # include Sprockets::Helpers::RailsHelper
+  # include Sprockets::Helpers::IsolatedHelper
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :format_conversion, :if => :is_caf_file? do
+    process :resample => "128k"
+  end
+
+  def is_caf_file? asset
+    asset.extension.downcase == "caf"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+   def filename
+debugger
+     old_extension = File.extname(super)
+     if old_extension.present? and old_extension.downcase == ".caf"
+       super.chomp(File.extname(super)) + '.WAV'
+     else
+       super
+     end
+   end
+
+end
---
+++ lib/carrier_wave/ffmpeg.rb
@@ -0,0 +1,23 @@
+# lib/carrierwave/ffmpeg.rb
+require 'streamio-ffmpeg'
+module CarrierWave
+  module FFMPEG
+    module ClassMethods
+      def resample( bitrate )
+        process :resample => bitrate
+      end
+    end
+ 
+    def resample( bitrate )
+debugger
+      directory = File.dirname( current_path )
+      tmp_path   = File.join( directory, "tmpfile" )
+      FileUtils.mv( current_path, tmp_path )
+      file = ::FFMPEG::Movie.new(tmp_path)
+      file.transcode(directory + "/format_conversion_#{filename}")
+
+      File.delete( tmp_path )
+
+    end
+  end
+end
